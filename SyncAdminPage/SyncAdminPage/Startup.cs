11ef0854326c27cd25e7a0e2d6f60080095c05fa
Lindsay Spencer <lindsay.m.spencer@gmail.com>
2018-02-11 21:47:20 -0800

    Setup part 2.

---
+++ SyncAdminPage/SyncAdminPage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SyncAdminPage.Startup))]
+namespace SyncAdminPage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
